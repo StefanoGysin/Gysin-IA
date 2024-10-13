3c1e9c16b8bc2060419ef52667bfb0aebd417bcb
StefanoGysin <stefanogysin@hotmail.com>
2024-10-13 15:26:51 +0200

    Adiciona teste para funcionalidades de mapa mental

--- tests/test_modelo_linguagem.py
+++ tests/test_modelo_linguagem.py
@@ -1,5 +1,6 @@
 # tests/test_modelo_linguagem.py
 
+import os
 import unittest
 from core.language_model.modelo_linguagem import ModeloLinguagem
 from utils.exceptions import ModeloLinguagemError
@@ -45,6 +46,22 @@ class TestModeloLinguagem(unittest.TestCase):
         
         with self.assertRaises(ModeloLinguagemError):
             self.modelo.processar_texto(None)
+
+    def test_mapa_mental(self):
+        conceito = "Python"
+        relacionados = ["Programação", "Linguagem", "Orientação a Objetos"]
+        self.modelo.adicionar_ao_mapa_mental(conceito, relacionados)
+        
+        self.modelo.gerar_mapa_mental("test_mapa_mental.png")
+        self.assertTrue(os.path.exists("test_mapa_mental.png"))
+        
+        conceitos_relacionados = self.modelo.gerador_mapa.obter_conceitos_relacionados(conceito)
+        self.assertEqual(set(conceitos_relacionados), set(relacionados))
+
+    def tearDown(self):
+        # Remover o arquivo de teste após cada teste
+        if os.path.exists("test_mapa_mental.png"):
+            os.remove("test_mapa_mental.png")
 
 if __name__ == '__main__':
     unittest.main()
